10278b32a12c82f3c18d2ca2ac4a0661d3714776
pinkishsabito <ozhetov.arsen@gmail.com>
2024-04-29 03:02:00 +0500

    feat: swagger config (refactored, not working)

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Course Platform')
+    .setDescription('Johny ozi biled (NA)')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3001);
 }
+
 bootstrap();
