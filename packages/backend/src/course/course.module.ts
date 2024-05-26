81a18488fbc3ba96129690383a32db62a973e7e4
pinkishsabito <ozhetov.arsen@gmail.com>
2024-05-27 00:13:35 +0300

    style: otirik birdene

--- packages/backend/src/course/course.module.ts
+++ packages/backend/src/course/course.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CourseController } from './course.controller';
 import { CourseService } from './course.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [CourseController],
