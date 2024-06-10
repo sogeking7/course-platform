eda39b1969fb90774bb64e96d6c7f33785802793
sogeking7 <kairolla222@gmail.com>
2024-06-10 19:14:55 +0500

    add /home to middleware

--- packages/frontend/src/middleware.ts
+++ packages/frontend/src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/home/profile/:path*", "/admin/:path*", "/course/:id/learning/:path*", ],
+  matcher: ["/home/:path*", "/admin/:path*", "/course/:id/learning/:path*", ],
 };
